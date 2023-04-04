e928c64687b1b7dbcccca77905c57500de1bb207
Gustavo Carrillo (He/Him) <115580058+0zrk23@users.noreply.github.com>
2023-04-03 22:24:11 -0600

    curiosity

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,20 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
-  {
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-    port: process.env.MYSQLPORT
-  }
-)
+const sequelize = new Sequelize(process.env.MYSQL_URL, {
+  dialect: 'mysql',
+});
+
+// const sequelize = new Sequelize(
+//   process.env.MYSQLDATABASE,
+//   process.env.MYSQLUSER,
+//   process.env.MYSQLPASSWORD,
+//   {
+//     host: process.env.MYSQLHOST,
+//     dialect: 'mysql',
+//     port: process.env.MYSQLPORT
+//   }
+// )
 
 // DEV PURPOSES
 // const sequelize = new Sequelize(
